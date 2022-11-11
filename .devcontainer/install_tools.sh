5109fe42df87e791464a8b58c49fe0b6c1fe9f5b
Øyvind Brakstad <brakistad@gmail.com>
2022-11-11 12:46:57 +0000

    finalize

--- .devcontainer/install_tools.sh
+++ .devcontainer/install_tools.sh
@@ -6,17 +6,5 @@
 set -e
 
 # Install tools
-# Gopls is the language server for Go
-go get -u golang.org/x/tools/gopls@latest
-# golangci-lint is a linter for Go
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-# goimports is a tool for organizing imports
-go get -u golang.org/x/tools/cmd/goimports
-# gocode is an autocompletion tool for the Go language
-go get -u github.com/mdempsky/gocode
-# gorename is a tool for renaming identifiers in Go source code
-go get -u golang.org/x/tools/cmd/gorename
-# go-delve is a debugger for the Go programming language
-go get -u github.com/go-delve/delve/cmd/dlv
 # air is a live reload tool for Go apps
 go install github.com/cosmtrek/air@latest
